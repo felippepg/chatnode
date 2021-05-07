3191c548fc1dfcb1307eaf4694ef73f4a8bc7a48
Felippe Pires <fpires.fga@gmail.com>
2021-05-07 13:36:21 -0300

    Adicionando as mensagem de aviso no Chat

--- public/assets/script/main.js
+++ public/assets/script/main.js
@@ -11,12 +11,59 @@ let chatTextInput = document.querySelector('#chatTextInput');
 loginPage.style.display = 'flex';
 chatPage.style.display = 'none';
 
+function renderUserList() {
+    let ul = document.querySelector('.userList');
+    ul.innerHTML = '';
+
+    userList.forEach(user => {
+        ul.innerHTML += '<li>' + user + '</li>';
+    })
+}
+
+function addMessage(type, user, msg) {
+    let ul = document.querySelector('.chatList');
+
+    switch (type) {
+        case 'status':
+            ul.innerHTML += '<li class="m-status">' + msg + '</li>';
+            break;
+
+        case 'msf':
+            ul.innerHTML += '<li class="m-txt"><spam>' + user + '</spam>' + msg + '</li>';
+            break;
+    }
+}
+
 loginNameInput.addEventListener('keyup', (e) => {
     if (e.keyCode === 13) {
         let name = loginNameInput.value.trim();
         if (name != '') {
             userName = name;
             document.title = 'Chat (' + userName + ')';
+            socket.emit('join-user', userName);
         }
     }
+})
+
+socket.on('user-ok', (list) => {
+    loginPage.style.display = 'none';
+    chatPage.style.display = 'flex';
+    userList = list;
+
+    addMessage('status', null, 'Conectado');
+    renderUserList();
+});
+
+socket.on('update-users', (data) => {
+    if (data.join) {
+        addMessage('status', null, data.join + ' entrou no Chat.');
+    };
+
+    if (data.left) {
+        addMessage('status', null, data.left + ' saiu do Chat.');
+    };
+
+    userList = data.list;
+
+    renderUserList();
 })
--- server.js
+++ server.js
@@ -10,6 +10,29 @@ const io = socktIO(server);
 server.listen(3000);
 app.use(express.static(path.join(__dirname, 'public')));
 
+let connectedUsers = [];
+
 io.on('connection', (socket) => {
     console.log('Conexão criada com sucesso');
+
+    socket.on('join-user', (username) => {
+        socket.username = username;
+        connectedUsers.push(username);
+        console.log(connectedUsers);
+
+        socket.emit('user-ok', connectedUsers);
+        socket.broadcast.emit('update-users', {
+            join: username,
+            list: connectedUsers
+        });
+    });
+
+    socket.on('disconnect', () => {
+        connectedUsers = connectedUsers.filter(user => user != socket.username);
+        console.log(connectedUsers);
+        socket.broadcast.emit('update-users', {
+            left: socket.username,
+            list: connectedUsers
+        });
+    });
 });
